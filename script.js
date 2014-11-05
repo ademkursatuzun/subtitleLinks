e8dd3494491cde29d907e0be07cf1353b1f02aec
Adem Kürşat Uzun <ademkursatuzun@gmail.com>
2014-11-05 23:01:34 +0200

    Added getTitle function.
    That function is getting title of movie.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // ==UserScript==
-// @name        subtitleLinks
-// @namespace   akursat.com/script
-// @include     http://www.imdb.com/title/*
-// @version     v1.0
-// @grant       none
-// @author      akursat
+// @id             www.imdb.com-5763720c-0524-4b86-af79-87e6136cb253@scriptish
+// @name           IMDb kat.ph and Torrentz Links and yts.re
+// @version        1.2
+// @namespace      
+// @author         xpdite
+// @author         akursat
+// @description    
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @include        http://www.imdb.com/title/*
+// @run-at         document-end
 // ==/UserScript==
+
+/* @require annotation need version 0.8. You must sure running with Greasemonkey version 0.8.
+ */
+
+//Avoiding some conflit.
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+$( document ).ready(function() {
+    //Get title of Movie
+    function getTitle(){
+       var title = $('h1.header').children('.itemprop').text();
+       return title;
+    }
+});
