6942bf46206a03ee048d717f96bff8067fa3a317
Adem Kürşat Uzun <ademkursatuzun@gmail.com>
2014-11-05 13:48:37 +0200

    Created script.js
    greasemonkey meta-tags are added.

---
+++ script.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        subtitleLinks
+// @namespace   akursat.com/script
+// @include     http://www.imdb.com/title/*
+// @version     v1.0
+// @grant       none
+// @author      akursat
+// ==/UserScript==
